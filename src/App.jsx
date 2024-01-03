6fcc9200ca7b2bd6dc3f5643843e9df090be862f
Karan-Palan <karanpalan007@gmail.com>
2024-01-03 11:58:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+const App = () => (
+  <main>
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      Hero
+    </section>
+    <section className="padding">
+      PopolarProducts
+    </section>
+    <section className="padding">
+      SuperQuality
+    </section>
+    <section className="padding">
+      Services
+    </section>
+    <section className="padding">
+      SpecialOffer
+    </section>
+  </main>
+);
+
+export default App;
